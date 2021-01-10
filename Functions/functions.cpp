c8fb752caf072d0b3c8809d55a806a3c4da7fa94
John Foster <johnfost.361@gmail.com>
2021-01-09 20:08:26 -0500

    Recursion added

--- Functions/functions.cpp
+++ Functions/functions.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <array>
+#include <iomanip>
 using namespace std;
 
 void function(int); //this is a prototype, which allows the function to be used
 double calc_cost(double base_cost, double tax_rate = .06); //tax_rate is a default, it needs to come at the tail end
 void print_array(int[], size_t);
+
+double a_penny_doubled_everyday(int, double);
+int function_activation_count{ 0 };
 
 int main() {
 	//C++ needs to have a prototype for each function before it is used. the prototype does not need to know the implementation, just the function header and the types of parameters it needs
@@ -36,6 +40,11 @@ int main() {
 	//inline functions allow for assembly code to avoid call overhead, but this is usually controlled by the compiler automatically
 
 	//recursion
+		//the stack builds up until the base case is met, then it works backwards, going from the base case to the original iteration using the value that was used in the function call
+		//the base case is used to end the recursion back stepping
+
+	double total_amount = a_penny_doubled_everyday(18, .01);
+	cout << "If I start with a penny and doubled it every day for 25 days, I will have $" << setprecision(10) << total_amount;
 }
 
 void function(int x) {
@@ -50,4 +59,19 @@ void print_array(int arr[], size_t size) { //when an array is passed into a func
 	for (int i = 0; i < size; i++) {
 		cout << arr[i] << endl;
 	}
+}
+
+//recursion
+double a_penny_doubled_everyday(int days, double money) {
+	function_activation_count++;
+	double total_amount{ 0 };
+	if (days == 1) {
+		return money;
+	}
+	total_amount = total_amount + a_penny_doubled_everyday((days - 1), (money * 2));
+	return total_amount;
+}
+
+int test_function_activation_count() {
+	return function_activation_count;
 }
